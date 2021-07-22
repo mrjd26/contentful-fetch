174d63683ba365cd03c3006d6051ae5a0385e47c
Mike <mkejrvs@gmail.com>
2021-07-21 21:02:16 -0500

    Contenful & fetch & Create-react-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+const {REACT_APP_SPACE_TOKEN, REACT_APP_ACCESS_TOKEN} = process.env;
+
 function App() {
+
+
+  fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_TOKEN}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${REACT_APP_ACCESS_TOKEN}`
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+
+          post(id: "5WH0joUHPCaC9gk3Hzpq3f") {
+            title
+            subtitle
+            slug
+          }
+  
+          assetCollection  {
+            items  {
+              title
+              description
+              fileName
+            }
+         }
+  
+          postCollection {
+            items {
+              title
+              slug
+              contentfulMetadata {
+                tags {
+                  id
+                }
+              }
+              content{
+                json
+              }
+            }
+          }
+        }    
+      `
+    })
+  })
+    .then((res) => res.json())
+    .then((result) => console.log(result));
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
