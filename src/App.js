c2315dc64bd0cae8a0185ddea3790e62cbc2a277
Mike <mkejrvs@gmail.com>
2021-07-28 12:40:03 -0500

    homepage and articles loading and rendering data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-
+import  React from 'react';
 import './App.css';
 import useContentful from './hooks/use-contentful.js';
-
-  var query =
+import { AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Typography, Container} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+} from "react-router-dom";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+   const classes = makeStyles((theme) => ({
+       icon: {
+           marginRight: theme.spacing(2),
+       },
+       heroContent: {
+           backgroundColor: theme.palette.background.paper,
+           padding: theme.spacing(8, 0, 6),
+       },
+       heroButtons: {
+           marginTop: theme.spacing(4),
+       },
+       cardGrid: {
+           paddingTop: theme.spacing(8),
+           paddingBottom: theme.spacing(8),
+       },
+       card: {
+           height: '100%',
+           display: 'flex',
+           flexDirection: 'column',
+       },
+       cardMedia: {
+           height: 150,
+           paddingTop: '56.25%', // 16:9
+       },
+       cardContent: {
+           flexGrow: 1,
+       },
+       footer: {
+           backgroundColor: theme.palette.background.paper,
+           padding: theme.spacing(6),
+       },
+   }));
+   
+var query =
  `
 query {
-  postCollection {
+  postCollection (limit:9) {
     items {
       sys {
         id
+        firstPublishedAt
       }
       title
       subtitle
@@ -17,65 +60,201 @@ query {
         title
         description
         contentType
-        fileName
-        size
-        url(transform: {
-          height: 300
-          width: 300
-        })
-        width
-        height
-      }
-      content {
-        json
+        url
       }
       contentfulMetadata {
         tags {
           id
+          name
         }
       }
     }
   }
 }  
- `;
-
-
-function App() {
-
-  let {data, errors} = useContentful(query);
-  console.log(errors);
-  if (errors) return (
-
-    <span style={{color: "red"}}>
-      {errors.map((error) => error.message).join(",")}
-    </span>
-
-  );
-
-  if (!data) return <span> Loading ... </span>
-console.log(data);
-  return (
-        <body>
-          <div>
-            {data.postCollection.items.map((article, index) => (
-                <li>
-                    <a href="{article.slug}"><div className="article" style={{
-                        height: 300,
-                        width: 300,
-                        backgroundImage: `url(${article.image.url})`
-                    }}>
-                    </div></a>
-                    <br/>
-                    <br/>
-                    <a href="{article.slug}"><span className="button-51" role="button">{article.title}</span></a>
-                    <br/>
-                    <br/>
-                     -------------------------------------------------------------------
-                </li>
-            ))}
-          </div>
-        </body>
-  );
+ `
+function cleanTimestamp(unix_ts) {
+  let date = new Date(unix_ts);
+  return date.toDateString();
 }
 
-export default App;
+function Home() {
+
+  let {data, errors} = useContentful(query) ;
+
+  if (!data) {
+      return <span> Loading ... </span>
+  }   else {
+
+      return (
+          <React.Fragment>
+              <CssBaseline/>
+              <AppBar position="relative">
+                  <Toolbar color="#cfe8fc">
+
+                      <Typography variant="body1" color="inherit" noWrap>
+                          <Link to="/">Home</Link>
+                          <Link style={{marginLeft: 55, color:"white"}} to="/about">About</Link>
+
+                      </Typography>
+                  </Toolbar>
+              </AppBar>
+
+
+              <main>
+                  {/* Hero unit */}
+                  <div className={classes.heroContent}>
+
+                      <Container maxWidth="sm">
+                          <div className={classes.heroButtons}>
+                              <Grid container spacing={2} justifyContent="center">
+                                  <Grid item>
+
+                                  </Grid>
+                              </Grid>
+                          </div>
+                      </Container>
+                  </div>
+
+                  <Container className={classes.cardGrid} maxWidth="md">
+                      {/* End hero unit */}
+                      <Grid container spacing={4}>
+                          {data.postCollection.items.map((article) => (
+
+                              <Grid item key={article.slug} xs={12} sm={6} md={4}>
+
+                                  <Card className={classes.card}>
+
+                                      <Link to={`/${article.slug}`}>
+
+                                          <CardMedia
+                                              component="img"
+                                              className={classes.cardMedia}
+                                              image={article.image.url}
+                                              title={article.title}
+                                          />
+                                      </Link>
+
+                                      <CardContent className={classes.cardContent}>
+                                          <Typography gutterBottom variant="body2">
+                                              {article.title}
+                                          </Typography>
+                                          <Typography variant="caption">
+                                              {article.subtitle}
+                                          </Typography>
+                                      </CardContent>
+                                      <CardActions>
+                                          <Button size="small" color="primary">
+                                              {cleanTimestamp(article.sys.firstPublishedAt)}
+                                          </Button>
+                                          <Button size="small" color="primary">
+                                              {article.contentfulMetadata.tags.name}
+                                          </Button>
+                                      </CardActions>
+                                  </Card>
+                              </Grid>
+
+                          ))}
+
+                      </Grid>
+                  </Container>
+
+              </main>
+              {/* Footer */}
+              <footer className={classes.footer}>
+                  <Typography variant="h6" align="center" gutterBottom>
+                      I
+                  </Typography>
+                  <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                      Copywrite 2021
+                  </Typography>
+
+              </footer>
+              {/* End footer */}
+          </React.Fragment>
+      );
+  }
+}
+
+function About() {
+    console.log('hello,Im out and about')
+    return <h1> This is all about me baby</h1>
+}
+
+function Template() {
+    let { article } = useParams();
+
+    let query = `
+    query($variable_name: String) {
+      postCollection(where:{slug:$variable_name}) {
+        items {
+          sys {
+            id
+            firstPublishedAt
+          }
+          slug
+          title
+          subtitle
+          content {
+            json
+          }
+          contentfulMetadata {
+            tags {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+    `
+    const variables = {"variable_name": article};
+    let {data, errors} = useContentful(query, variables) ;
+
+    return (
+        <React.Fragment>
+          <CssBaseline/>
+          <AppBar position="relative">
+              <Toolbar>
+
+                  <Typography variant="h6" color="inherit" noWrap>
+                      <Link to="/">Home</Link>
+                      <Link style={{marginLeft: 55}} to="/about">About</Link>
+
+                  </Typography>
+              </Toolbar>
+           </AppBar>
+
+          <div className={classes.heroContent}>
+             <Container maxWidth="sm">
+                 <Typography component="div" style={{backgroundColor: '#cfe8fc'}} >
+                   { data ? (
+                     <div>{
+                      documentToReactComponents(data.postCollection.items[0].content.json)
+                     } </div>
+                       ) : (
+                     <p>`...Loading {article}`</p>
+                       )}
+                 </Typography>
+             </Container>
+          </div>
+        </React.Fragment>
+    );
+}
+
+export const App = () => {
+    return (
+        <Router>
+          <Switch>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/:article">
+                  <Template />
+              </Route>
+          </Switch>
+        </Router>
+    )
+}
--- src/hooks/use-contentful.js
+++ src/hooks/use-contentful.js
@@ -3,29 +3,55 @@ import {useState, useEffect} from 'react';
 const {REACT_APP_SPACE_TOKEN, REACT_APP_ACCESS_TOKEN} = process.env;
 
 
-function useContentful(query) {
+function useContentful(query, variables) {
 
   let [data, setData] = useState(null);
   let [errors, setErrors] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_TOKEN}`, {
+  async function getData(query, variables){
+    const url = `https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_TOKEN}`;
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${REACT_APP_ACCESS_TOKEN}`
       },
-      body: JSON.stringify({query})
+      body: JSON.stringify({query, variables})
+    });
+    return {data, errors} = await response.json();
+  }
+
+  useEffect(() => {
+    {/*
+    return window.fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_TOKEN}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${REACT_APP_ACCESS_TOKEN}`
+      },
+      body: JSON.stringify({query, variables})
     })
     .then((response) => response.json())
     .then(({data, errors}) => {
-          if (errors) setErrors(errors);
+          if (errors) {
+            setErrors(errors);
+            console.log(errors)
+          }
           if (data) setData(data);
     })
-    .catch(error => setErrors([errors])); 
-  }, [query]);
+    .catch(error => setErrors([errors])); */}
+    const loadData = async () => {
+      let {data, errors} = await getData(query,variables);
+      if(data) setData(data)
+      if(errors) setErrors(errors)
+    };
+    setTimeout(() => {
+      loadData();
+    }, 1500);
 
-  return {data, errors};
+  }, [query]);
+  console.log('23', data,errors);
+  return {data, errors}
 }
 
 export default useContentful;
